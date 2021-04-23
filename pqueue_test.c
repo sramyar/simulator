55159028e5d80ebac7778d4dc7eaba2ae59f1830
Sepehr <sepehr.ramyar@gmail.com>
2021-04-22 17:45:34 -0700

    completed unit tests for queue and priority queue

--- pqueue_test.c
+++ pqueue_test.c
@@ -1,5 +1,12 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
-//#include"Heap.c"
+#include <signal.h>
+#include <setjmp.h>
+#include <unistd.h> 
+
 #include"PriorityQueue.c"
 
 
@@ -7,7 +14,9 @@
 #define MAXSCORE 40
 
 static uint8_t testsPassed;
-int testStatus;
+static volatile sig_atomic_t testStatus;
+static uint8_t disable_exit_handler;
+jmp_buf test_crash;
 
 enum Test_enumerator{
     Empty_heap = 0,
@@ -113,21 +122,72 @@ uint8_t runTest(Heap* h, int test)
     return 255;
 }
 
+void segfault_handler(int signal) { // everyone knows what this is
+  testStatus = 255;
+  longjmp(test_crash, 1);
+}
 
-int main(void)
+void exit_attempt_handler(void) { // only I decide when you are done
+  if (disable_exit_handler) return; // allow this to be disabled
+  testStatus = 255;
+  longjmp(test_crash, 2);
+}
+
+void abrupt_termination_handler(int signal) { // program killed externally
+  testStatus = 255;
+  longjmp(test_crash, 3);
+}
+
+int main(int argc, char **argv)
 {
+    if (argc > 2 || (argc == 2 && strcmp(argv[1], "-v") != 0)) {
+    printf("Usage: %s [-v]", (argc > 0 ? argv[0] : "./queue_test"));
+    exit(1);
+    }
+
+    printf("\n"); // more spacing
+    if (argc == 2) printf("\n"); // consistency in verbose mode
+
     testsPassed = 0;
-    int arr[10];
+    disable_exit_handler = 0;
+    atexit(exit_attempt_handler);
+    signal(SIGSEGV, segfault_handler);
+
+    int arr[10]; // initializing array for implementation of heap
     for (uint8_t i = 0; i < NUM_TESTS; i++){
         Heap* h = initializeHeap(10, 0, arr);
         testStatus = runTest(h, i);
         destructHeap(h);
 
+        uint8_t fail_type = setjmp(test_crash);
+        if (argc == 2) { // it's verbose mode
+            printf("Test %s: %s", testName(i), testStatus == 0 ? "PASSED" : "FAILED");
+            if (testStatus == 255) {
+                printf(": due to a %s\n", fail_type == 1 ? "segfault" : fail_type == 2 ? "program exit" : "program interruption");
+                printf("\nWARNING: Program will now stop running tests\n\n");
+            break;
+            } else if (testStatus != 0) {
+                printf(": test %d\n", testStatus);
+            } else {
+                printf("\n");
+                }
+        }
+
         if (testStatus == 0) testsPassed++;
-        else printf("\nFailed %s\n", testName(i));
     }
 
+    disable_exit_handler = 1;
+
     uint8_t totalScore = testsPassed*8;
+
+    if (argc == 2 ) printf("\nYou passed %d out of %d tests\n", testsPassed,
+      NUM_TESTS);
+
+    else if (testStatus == 255) {
+        if (totalScore < 10) {totalScore = 10;}
+        //totalScore = 10; // charity points
+        if (argc == 2) printf("Receiving charity points because your program crashes\n");
+    }
 
     printf("\nYou get %d out of %d for heap unit test\n", totalScore, MAXSCORE);
     exit(0);
--- queue_test.c
+++ queue_test.c
@@ -1,8 +1,11 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<stdint.h>
-#include <unistd.h> 
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 #include <signal.h>
+#include <setjmp.h>
+#include <unistd.h> 
 
 #include"Queue.c"
 
@@ -10,8 +13,9 @@
 #define MAXSCORE 60
 
 static uint8_t testsPassed;
-
-int testStatus;
+static volatile sig_atomic_t testStatus;
+static uint8_t disable_exit_handler;
+jmp_buf test_crash;
 
 
 enum Test_enumerator {
@@ -90,18 +94,72 @@ uint8_t runTest(Queue* q, int test)
 }
 
 
-int main(void)
+void segfault_handler(int signal) { // everyone knows what this is
+  testStatus = 255;
+  longjmp(test_crash, 1);
+}
+
+void exit_attempt_handler(void) { // only I decide when you are done
+  if (disable_exit_handler) return; // allow this to be disabled
+  testStatus = 255;
+  longjmp(test_crash, 2);
+}
+
+void abrupt_termination_handler(int signal) { // program killed externally
+  testStatus = 255;
+  longjmp(test_crash, 3);
+}
+
+int main(int argc, char **argv)
 {
+    if (argc > 2 || (argc == 2 && strcmp(argv[1], "-v") != 0)) {
+    printf("Usage: %s [-v]", (argc > 0 ? argv[0] : "./queue_test"));
+    exit(1);
+    }
+
+    printf("\n"); // more spacing
+    if (argc == 2) printf("\n"); // consistency in verbose mode
+
     testsPassed = 0;
-    int arr[4];
+    disable_exit_handler = 0;
+    atexit(exit_attempt_handler);
+    signal(SIGSEGV, segfault_handler);
+
+    int arr[4]; // initializing array used for implementing queue
     for (uint8_t i = FIRST_TEST; i <  NUM_TESTS; i++){
         Queue* q = initializeQueue(4, arr);
         testStatus = runTest(q,i);
         destructQueue(q);
+
+        uint8_t fail_type = setjmp(test_crash);
+        if (argc == 2) { // it's verbose mode
+            printf("Test %s: %s", testName(i), testStatus == 0 ? "PASSED" : "FAILED");
+            if (testStatus == 255) {
+                printf(": due to a %s\n", fail_type == 1 ? "segfault" : fail_type == 2 ? "program exit" : "program interruption");
+                printf("\nWARNING: Program will now stop running tests\n\n");
+            break;
+            } else if (testStatus != 0) {
+                printf(": test %d\n", testStatus);
+            } else {
+                printf("\n");
+                }
+        }
+
         if (testStatus == 0) testsPassed++;
     }
 
+    disable_exit_handler = 1;
+
     uint8_t totalScore = testsPassed*20;
+
+    if (argc == 2 ) printf("\nYou passed %d out of %d tests\n", testsPassed,
+      NUM_TESTS);
+
+    else if (testStatus == 255) {
+        if (totalScore < 10) {totalScore = 10;}
+        //totalScore = 10; // charity points
+        if (argc == 2) printf("Receiving charity points because your program crashes\n");
+    }
 
     printf("\nYou will receive %d out of %d possible points on the unit tests\n\n",totalScore, MAXSCORE);
     exit(0);
